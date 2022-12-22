561ec686c1a8ce517006fa27766322430e7381fe
coachkim <kimsw0825@gmail.com>
2022-12-22 16:48:33 +0900

    Docs : git 폴더정리

--- src/shared/router.js
+++ src/shared/router.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "../redux/components/Header";
-import Footer from "../redux/components/Footer";
+import Header from "../redux/components/Header/Header";
+import Footer from "../redux/components/Footer/Footer";
 import "./router.css";
 
 const Router = () => {
