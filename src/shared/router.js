945d1a2029c9abe98ebf63f02c879fac329b348d
daeho sim <tlaeogh5@gmail.com>
2022-12-22 21:35:52 +0900

    헤더부분완성 저장

--- src/shared/router.js
+++ src/shared/router.js
@@ -1,25 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
-import Header from "../redux/components/Header";
-import Community from "../pages/Community";
-import Detail from "../pages/Detail";
-=======
 import Header from "../redux/components/Header/Header";
 import Footer from "../redux/components/Footer/Footer";
 import "./router.css";
->>>>>>> b627fc0da10a30eee726eb61517bbbe44fbbfa75
 
 const Router = () => {
   return (
     <BrowserRouter>
-<<<<<<< HEAD
-      <Header />
-      <Routes>
-        <Route path="/123" element={<Community />} />
-        <Route path="/" element={<Detail />} />
-      </Routes>
-=======
       <div className="wrapper">
         <div className="contentWrapper">
           <Header />
@@ -44,7 +31,6 @@ const Router = () => {
         </div>
         <Footer />
       </div>
->>>>>>> b627fc0da10a30eee726eb61517bbbe44fbbfa75
     </BrowserRouter>
   );
 };
