1335b32500778c3b2efff709663ab443f9d3d5b8
daeho sim <tlaeogh5@gmail.com>
2022-12-22 21:20:08 +0900

    Feat: 헤더에서 커뮤니티 이동

---
+++ src/pages/Community.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Community() {
+  return <div>이제 여기에 데이터베이스 불러오면 끝인가요?</div>;
+}
--- src/shared/router.js
+++ src/shared/router.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "../redux/components/Header";
+import Community from "../pages/Community";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" />
+        <Route path="/123" element={<Community />} />
       </Routes>
     </BrowserRouter>
   );
