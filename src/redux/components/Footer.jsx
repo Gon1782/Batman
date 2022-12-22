bfe639120e8c55dbb0ffbef72baee01c96ac1aa8
coachkim <kimsw0825@gmail.com>
2022-12-22 16:27:24 +0900

    Feat: git Header 추가

---
+++ src/redux/components/Footer.jsx
@@ -0,0 +1,15 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div>
+        <p>상호명</p>
+        <p>회사위치 : 고담시</p>
+      </div>
+      <p>TEL.1336</p>
+    </div>
+  );
+}
+
+export default Footer;
--- src/shared/router.js
+++ src/shared/router.js
@@ -1,12 +1,36 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "../redux/components/Header";
+import Footer from "../redux/components/Footer";
+import "./router.css";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" />
-      </Routes>
+      <div className="wrapper">
+        <div className="contentWrapper">
+          <Header />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <div>메인페이지</div>
+                </>
+              }
+            />
+            <Route
+              path="/community"
+              element={
+                <>
+                  <div>커뮤니티</div>
+                </>
+              }
+            />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 };
