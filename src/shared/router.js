8c1cd840733017eda6b4ebd10898542b03c590ac
coachkim <kimsw0825@gmail.com>
2022-12-23 09:14:19 +0900

    css header 고정

--- src/shared/router.js
+++ src/shared/router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "../redux/components/Header/Header";
 import Footer from "../redux/components/Footer/Footer";
+import Detail from "../pages/Detail";
 import "./router.css";
 
 const Router = () => {
@@ -27,6 +28,7 @@ const Router = () => {
                 </>
               }
             />
+            <Route path="/detail" element={<Detail />} />
           </Routes>
         </div>
         <Footer />
