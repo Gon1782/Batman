01ce4be33b72f564c76259e3f69a22f02a2c175d
Leekee01 <zeje23@naver.com>
2022-12-23 23:13:48 +0900

    modal

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import Bet from '../redux/components/bet/Bet';
-import Comment from '../redux/components/comment/Comment';
-import Match from '../redux/components/match/Match';
-import "../styles/detail.css"
-
+import Bet from "../redux/components/bet/Bet";
+import Comment from "../redux/components/comment/Comment";
+import Match from "../redux/components/match/Match";
+import "../styles/detail.css";
 const Detail = () => {
   return (
     <div className="detail_layout">
