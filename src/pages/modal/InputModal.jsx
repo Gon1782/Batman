d4b9099422febe2f99b40c39f85b0f4fb8ba7679
Leekee01 <zeje23@naver.com>
2022-12-23 11:09:45 +0900

    Feat: "모달"

---
+++ src/pages/modal/InputModal.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const InputModal = ({ show, onHide }) => {
+  const [posts, setPosts] = useState({
+    title: "",
+    contents: "",
+  });
+
+  const onChangeText = (e) => {
+    setPosts(e.target.value);
+  };
+
+  const onSubmitHandler = async (posts) => {
+    await axios.post("http://localhost:3001/todos", posts);
+
+    setPosts(data);
+  };
+
+  return (
+    <>
+      <Modal show={show} onHide={onHide} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+        <Container>
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">글쓰기</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={onSubmitHandler}>
+              {/* 글 작성 칸 */}
+              <FloatingLabel controlId="floatingTextarea2" label="제목을 적어주세요">
+                <Form.Control as="textarea" placeholder="작성해주세요" style={{ height: "250px" }} onChange={onChangeText} value={title} />
+              </FloatingLabel>
+              <FloatingLabel controlId="floatingTextarea2" label="내용을 적어주세요">
+                <Form.Control as="textarea" placeholder="작성해주세요" style={{ height: "250px" }} onChange={onChangeText} value={contents} />
+              </FloatingLabel>
+
+              {/* 글 올리기 버튼 */}
+              <Button type="submit" variant="info" className="my-3" style={{ width: "300px", margin: "auto", display: "block" }}>
+                올리기
+              </Button>
+            </Form>
+          </Modal.Body>
+        </Container>
+      </Modal>
+    </>
+  );
+};
+export default InputModal;
