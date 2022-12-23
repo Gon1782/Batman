c4d57970a1bd2dfaeeca38567b75f667c3937088
daeho sim <tlaeogh5@gmail.com>
2022-12-23 14:26:23 +0900

    모달연결할게요

--- src/shared/router.js
+++ src/shared/router.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "../redux/components/Header/Header";
 import Footer from "../redux/components/Footer/Footer";
-import Detail from "../pages/Detail";
 import "./router.css";
 import Community from "../pages/Community/Community";
 
@@ -13,28 +12,8 @@ const Router = () => {
         <div className="contentWrapper">
           <Header />
           <Routes>
-<<<<<<< HEAD
             <Route path="/" element={<></>} />
             <Route path="/123" element={<Community />} />
-=======
-            <Route
-              path="/"
-              element={
-                <>
-                  <div>메인페이지</div>
-                </>
-              }
-            />
-            <Route path="/detail" element={<Detail />} />
-            <Route
-              path="/community"
-              element={
-                <>
-                  <div>커뮤니티</div>
-                </>
-              }
-            />
->>>>>>> 166368a04cfefcbfe79ab4293838e9c10672c103
           </Routes>
         </div>
         <Footer />
