89f67d2d3d66f14bc95d35bd90889c0378aaf864
coachkim <kimsw0825@gmail.com>
2022-12-26 09:14:47 +0900

    Feat <Match /> 상세페이지 기능추가

--- src/pages/MainPage/LeagueInformation/Epl.jsx
+++ src/pages/MainPage/LeagueInformation/Epl.jsx
@@ -1,35 +1,35 @@
-import axios from 'axios';
-import { useQuery } from 'react-query';
-import './AllLeagues.css';
-import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+import { useQuery } from "react-query";
+import "./AllLeagues.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Epl() {
   const navigate = useNavigate();
-  const { isLoading, error, data } = useQuery(['information'], async () => await axios.get('http://localhost:3001/EPL').then((res) => res.data));
+  const { isLoading, error, data } = useQuery(["information"], async () => await axios.get("http://localhost:3001/EPL").then((res) => res.data));
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>error</p>;
 
   return (
-    <div className='information'>
-      {data.map((data) => (
+    <div className="information">
+      {data.map((data, i) => (
         <>
-          <div className='main_league_title' key={data.id}>
+          <div className="main_league_title" key={data.id}>
             {data.title}
           </div>
 
           <div
-            className='main_league'
+            className="main_league"
             onClick={() => {
-              navigate('/detail');
+              navigate("/detail/EPL" + i + "", { state: { EPLId: "EPL", EPLUId: parseInt("100" + i) } });
             }}
           >
-            <div className='epllist' key={data.id}>
-              <div className='home'>
-                {data.home} <img src={data.homeimg} alt='img' />
+            <div className="epllist" key={data.id}>
+              <div className="home">
+                {data.home} <img src={data.homeimg} alt="img" />
               </div>
-              <div className='game_time'>{data.time}</div>
-              <div className='away'>
-                <img src={data.awayimg} alt='img' />
+              <div className="game_time">{data.time}</div>
+              <div className="away">
+                <img src={data.awayimg} alt="img" />
                 {data.away}
               </div>
             </div>
--- src/redux/components/match/Match.jsx
+++ src/redux/components/match/Match.jsx
@@ -4,13 +4,47 @@ import eplLogo from "../../../assets/img/EPL_logo.png";
 import ENG from "../../../assets/img/eng.png";
 import Man_City from "../../../assets/img/Man_City.png";
 import Liverpool from "../../../assets/img/Liverpool.png";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams, useLocation } from "react-router-dom";
 import axios from "axios";
+// import { useQuery } from "react-query";
 
 const Match = () => {
+  const { path } = useParams();
+  const location = useLocation();
+  const EPLId = location.state.EPLId;
+  const LaligaId = location.state.LaligaId;
+  const SerieAId = location.state.SerieAId;
+  const EPLUId = location.state.EPLUId;
+  const [leagueUID, setLeagueUID] = useState(0);
+  console.log(EPLUId);
+  console.log("100" + 1);
+
+  const LeagueId = (i) => {
+    let leagueid;
+    if (path === "EPL" + i + "") {
+      setLeagueUID(i);
+      leagueid = EPLId;
+    } else if (path === "Laliga" + i + "") {
+      setLeagueUID(i);
+      leagueid = LaligaId;
+    } else if (path === "SerieA" + i + "") {
+      setLeagueUID(i);
+      leagueid = SerieAId;
+    }
+    return leagueid;
+  };
+
+  console.log(location.pathname);
+  console.log(leagueUID);
+
   const navigate = useNavigate();
 
   const [matchdatas, setMatchdatas] = useState([]);
+  const matchData = matchdatas.filter((data) => data.id === EPLUId).map((data) => <div key={data.id}>{data.home}</div>);
+
+  // const { isLoading, error, data } = useQuery(["info"], async () => await axios.get("http://localhost:3001/" + { uid } + "").then((res) => res.data));
+  // if (isLoading) return <p>Loading...</p>;
+  // if (error) return <p>error</p>;
 
   // const matchdata = async () => {
   //   const { data } = await axios.get("http://localhost:3001/matchdatas");
@@ -18,7 +52,21 @@ const Match = () => {
   // };
 
   useEffect(() => {
-    axios.get("http://localhost:3001/matchdatas").then((res) => setMatchdatas(res.data));
+    axios
+      .get("http://localhost:3001/" + LeagueId(leagueUID) + "")
+      .then((res) => setMatchdatas(res.data))
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
   }, []);
 
   return (
@@ -26,14 +74,12 @@ const Match = () => {
       <section className="match_header">
         {/* 경기정보의 상단 부분 */}
         <button className="match_btn" onClick={() => navigate("/")}>
-          &lt; 뒤로가기
+          &lt; 뒤로가기{path}
         </button>
         {/* 홈으로 돌아가는 버튼 */}
         <div className="league">
           <img alt="" width="22" height="22" src={eplLogo} />
-          {matchdatas.map((data) => {
-            return <div>{data.league}</div>;
-          })}
+          {matchData}
         </div>
 
         <div className="country">
@@ -44,7 +90,7 @@ const Match = () => {
       <section className="match_body">
         {/* 경기정보의 중단 부분 */}
         <div className="home">
-          <div>Manchester City</div>
+          <div>Manchester City{}</div>
           <img alt="" width="50" height="50" src={Man_City} />
         </div>
         <div className="match_middle">
