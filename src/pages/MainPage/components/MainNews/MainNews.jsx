9bf3f3b3f7493438aa2fea9f0796ea3bead16ef1
Minwoozzang <kminu0819@naver.com>
2022-12-26 14:57:13 +0900

    feat: 슬라이더 추가

--- src/pages/MainPage/components/MainNews/MainNews.jsx
+++ src/pages/MainPage/components/MainNews/MainNews.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import './MainNews.css';
-// import messi from '../../../../assets/img/messi.png';
-// import haaland from '../../../../assets/img/haaland.png';
 import SimpleImageSlider from 'react-simple-image-slider';
 
 export default function MainNews() {
-  const images = [{ url: 'https://images.fotmob.com/image_resources/playerimages/30981.png' }, { url: 'images/haaland.png' }];
+  const images = [
+    {
+      url: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA3MzBfMzEg%2FMDAxNjU5MTM4MjExODI5.R6orXFxODj8iXjnM6uKgE3M6ZacpUHJu6KkvwR4pDn4g.YQorRgcq4USUGpvWTgs7tzqYE7_KiTMqTSISeiVB7CAg.PNG.xtaiji83%2F254.png&type=a340',
+    },
+    {
+      url: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjEwMDFfNzQg%2FMDAxNjY0NjE2NjU2MTY5.a3qUFgIFfuhTcJipp8BdrHUd_cpTv4X8dwKhY8pUhK4g.DZ_DIdHcmVKAJJ7Fkv_doUPauFN-opyCFo3f4TfZ2Fkg.JPEG.olengelight%2F%25C0%25CC%25B9%25CC%25C1%25F6_08-min.jpg&type=a340',
+    },
+    {
+      url: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjEwMDhfMzQg%2FMDAxNjY1MjExMTQ0MjA0.-sOfWpID1y1rp4TcnIzt4M7RuSutQGYr8GRR-40k8Hkg.LYVdxfoebSiREQSXjhkEm11HxNP1x0eDO09wae4tdOQg.JPEG.psh6347%2Fh_57864258.jpg&type=a340',
+    },
+    {
+      url: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjExMTJfMjA4%2FMDAxNjY4MjQwNzQwNjIy.AdrCWkCKOtflUDC1leC2LKCmFCX__cbHs6PlIbn3Kqwg.RC4crs3dfpkBPFNL1KrMq6bQHN832RgRPadFX5rxJbkg.JPEG.skjung114%2FScreenshot%25A3%25DF20221112%25A3%25AD170638.jpg&type=a340',
+    },
+  ];
   const style = {
     width: '100%',
     height: '100%',
+    backgroundColor: 'white',
+    borderRadius: '30px',
   };
+
   return (
     <div className='news_body'>
-      <div className='news_title'>Tending photos</div>
+      <div className='news_title'>Trending photos</div>
       <div className='news_main'>
-        <div className='news_img'>
-          <SimpleImageSlider width={`15%`} height={`30%`} style={style} images={images} showBullets={true} showNavs={true} />
-        </div>
+        <SimpleImageSlider width={`21%`} height={`32%`} style={style} images={images} showBullets={true} showNavs={true} />
       </div>
     </div>
   );
