aac2bf6ab7b55b4da6e2892e1e5c1cf8ab82293c
coachkim <kimsw0825@gmail.com>
2022-12-26 15:07:24 +0900

    docs: 상세페이지 기능 수정

--- src/redux/components/match/Match.jsx
+++ src/redux/components/match/Match.jsx
@@ -2,61 +2,63 @@ import React, { useState, useEffect } from "react";
 import "./match.css";
 import eplLogo from "../../../assets/img/EPL_logo.png";
 import ENG from "../../../assets/img/eng.png";
-import Man_City from "../../../assets/img/Man_City.png";
-import Liverpool from "../../../assets/img/Liverpool.png";
 import { useNavigate, useParams, useLocation } from "react-router-dom";
 import axios from "axios";
-// import { useQuery } from "react-query";
+// import Man_City from "../../../assets/img/Man_City.png";
+// import Liverpool from "../../../assets/img/Liverpool.png";
 
 const Match = () => {
   const { path } = useParams();
   const location = useLocation();
-  const EPLId = location.state.EPLId;
-  const LaligaId = location.state.LaligaId;
-  const SerieAId = location.state.SerieAId;
-  const EPLUId = location.state.EPLUId;
-  const LaligaUId = location.state.LaligaUId;
-  const SerieAUId = location.state.SerieAUId;
-  const [leagueUID, setLeagueUID] = useState(0);
-  console.log(path);
-  console.log(EPLUId);
-  console.log("100" + 1);
+  let EPLId = location.state.EPLId;
+  let LaligaId = location.state.LaligaId;
+  let SerieAId = location.state.SerieAId;
+  let EPLUId = location.state.EPLUId;
+  let LaligaUId = location.state.LaligaUId;
+  let SerieAUId = location.state.SerieAUId;
+  let t = path.replace(/[^0-9]/g, "");
 
   const LeagueId = (i) => {
     let leagueid;
     if (path === "EPL" + i + "") {
-      setLeagueUID(i);
       leagueid = EPLId;
     } else if (path === "Laliga" + i + "") {
-      setLeagueUID(i);
       leagueid = LaligaId;
     } else if (path === "SerieA" + i + "") {
-      setLeagueUID(i);
       leagueid = SerieAId;
     }
     return leagueid;
   };
 
-  console.log(location.pathname);
-  console.log(leagueUID);
+  const LeagueUId = (i) => {
+    let leagueuid;
+    if (path === "EPL" + i + "") {
+      leagueuid = EPLUId;
+    } else if (path === "Laliga" + i + "") {
+      leagueuid = LaligaUId;
+    } else if (path === "SerieA" + i + "") {
+      leagueuid = SerieAUId;
+    }
+    return leagueuid;
+  };
 
+  // console.log(location.pathname);
   const navigate = useNavigate();
 
   const [matchdatas, setMatchdatas] = useState([]);
-  const matchData = matchdatas.filter((data) => data.id === 1000).map((data) => <div key={data.id}>{data.home}</div>);
-
-  // const { isLoading, error, data } = useQuery(["info"], async () => await axios.get("http://localhost:3001/" + { uid } + "").then((res) => res.data));
-  // if (isLoading) return <p>Loading...</p>;
-  // if (error) return <p>error</p>;
-
-  // const matchdata = async () => {
-  //   const { data } = await axios.get("http://localhost:3001/matchdatas");
-  //   setMatchdatas(data);
-  // };
-
+  const matchData = matchdatas.filter((data) => data.id === LeagueUId(t)).map((data) => data.league);
+  const matchDatahome = matchdatas.filter((data) => data.id === LeagueUId(t)).map((data) => data.home);
+  const matchDatahomeimg = matchdatas.filter((data) => data.id === LeagueUId(t)).map((data) => data.homeimg);
+  const matchDataaway = matchdatas.filter((data) => data.id === LeagueUId(t)).map((data) => data.away);
+  const matchDataawayimg = matchdatas.filter((data) => data.id === LeagueUId(t)).map((data) => data.awayimg);
+  const matchDatahour = matchdatas.filter((data) => data.id === LeagueUId(t)).map((data) => data.hour);
+  const matchDatatime = matchdatas.filter((data) => data.id === LeagueUId(t)).map((data) => data.time);
+  const matchDataplace = matchdatas.filter((data) => data.id === LeagueUId(t)).map((data) => data.homeplace);
+  console.log(matchDatahomeimg);
+  console.log(matchData);
   useEffect(() => {
     axios
-      .get("http://localhost:3001/" + LeagueId(leagueUID) + "")
+      .get("http://localhost:3001/" + LeagueId(t) + "")
       .then((res) => setMatchdatas(res.data))
       .catch((error) => {
         if (error.response) {
@@ -76,40 +78,40 @@ const Match = () => {
     <div className="match_info">
       <section className="match_header">
         {/* 경기정보의 상단 부분 */}
-        <button className="match_btn" onClick={() => navigate("/")}>
-          &lt; 뒤로가기{path}
+        <button className="match_btn" onClick={() => navigate(-1)}>
+          &lt; 뒤로가기
         </button>
         {/* 홈으로 돌아가는 버튼 */}
         <div className="league">
-          <img alt="" width="22" height="22" src={eplLogo} />
+          <img alt="img" width="22" height="22" src={eplLogo} />
           {matchData}
         </div>
 
         <div className="country">
-          <img alt="" width="22" height="22" src={ENG} />
+          <img alt="img" width="22" height="22" src={ENG} />
           <div>ENG</div>
         </div>
       </section>
       <section className="match_body">
         {/* 경기정보의 중단 부분 */}
         <div className="home">
-          <div>Manchester City{}</div>
-          <img alt="" width="50" height="50" src={Man_City} />
+          <div>{matchDatahome}</div>
+          <img alt="img" src={matchDatahomeimg} />
         </div>
         <div className="match_middle">
-          <div>{}</div>
+          <div>{matchDatahour}</div>
           <div>vs</div>
         </div>
         <div className="away">
-          <img alt="" width="50" height="50" src={Liverpool} />
-          <div>Liverpool</div>
+          <img alt="img" width="50" height="50" src={matchDataawayimg} />
+          <div>{matchDataaway}</div>
         </div>
       </section>
       <section className="match_footer">
         {/* 경기정보의 하단부분 */}
-        <div>{}</div>
+        <div>{matchDatatime}</div>
         {/* 경기 시작시간 */}
-        <div>Etihad Stadium</div>
+        <div>{matchDataplace}</div>
         {/* 경기 장소 */}
       </section>
     </div>
