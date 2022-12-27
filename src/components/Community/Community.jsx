4290d7667f0d68d18bf6d0d67050d70c80f63b2c
Minwoozzang <kminu0819@naver.com>
2022-12-27 21:20:02 +0900

    pull

--- src/components/Community/Community.jsx
+++ src/components/Community/Community.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import "./Community.css";
-import Modal from "../modal/Modal";
-import { useDispatch, useSelector } from "react-redux";
-import { useQuery, useQueryClient, useMutation } from "react-query";
-import { changeCommunity, deleteCommunity, getCommunity } from "../../api/api";
-import { hideEdit, showEdit } from "../../redux/modules/communitySlice";
+import React, { useState } from 'react';
+import './Community.css';
+import Modal from '../modal/Modal';
+import { useDispatch, useSelector } from 'react-redux';
+import { useQuery, useQueryClient, useMutation } from 'react-query';
+import { changeCommunity, deleteCommunity, getCommunity } from '../../api/api';
+import { hideEdit, showEdit } from '../../redux/modules/communitySlice';
 
 export default function Community() {
   const CONFIRM_MESSAGE = `정말로 삭제하시겠습니까`;
@@ -19,13 +19,13 @@ export default function Community() {
 
   const deleteCommunityMutation = useMutation(deleteCommunity, {
     onSuccess: () => {
-      queryClient.invalidateQueries("community");
+      queryClient.invalidateQueries('community');
     },
   });
 
   const changeCommunityMutation = useMutation(changeCommunity, {
     onSuccess: () => {
-      queryClient.invalidateQueries("community");
+      queryClient.invalidateQueries('community');
     },
   });
 
@@ -63,7 +63,7 @@ export default function Community() {
 
   // GET community
 
-  const { isLoading, isError, data, error } = useQuery("community", getCommunity, {
+  const { isLoading, isError, data, error } = useQuery('community', getCommunity, {
     refetchOnWindowFocus: false,
     retry: 0,
   });
@@ -77,29 +77,29 @@ export default function Community() {
   }
 
   return (
-    <div className="wrap">
+    <div className='wrap'>
       <button
         onClick={() => {
           setModal(true);
         }}
-        className="community-Btn"
+        className='community-Btn'
       >
         작성하기
       </button>
       {modal && <Modal setModal={setModal} />}
       <div>
         {data.map((item) => (
-          <div className="divCommunity" key={item.id}>
-            <input readOnly={readOnly} defaultValue={item.title} onChange={onChangeCommunity} className="divTitle" />
-            <input readOnly={readOnly} defaultValue={item.contents} onChange={onChangeCotents} className="divContents" />
-            <div className="CompleteBtn">
+          <div className='divCommunity' key={item.id}>
+            <input readOnly={readOnly} defaultValue={item.title} onChange={onChangeCommunity} className='divTitle' />
+            <input readOnly={readOnly} defaultValue={item.contents} onChange={onChangeCotents} className='divContents' />
+            <div className='CompleteBtn'>
               {edit.onEdit && edit.id === item.id ? (
                 <button onClick={() => handleSuccessButtonClick(item.id, { title: updateCommunityInput, contents: updateContentsInput })}>완료</button>
               ) : (
                 <button onClick={() => handleModifyButtonClick(item.id)}>수정</button>
               )}
             </div>
-            <div className="CancelBtn">
+            <div className='CancelBtn'>
               {edit.onEdit && edit.id === item.id ? <button onClick={() => handleCancelButtonClick(item.id, item.title, item.contents)}>취소</button> : <button onClick={() => handleDeleteButtonClick(item.id)}>삭제</button>}
             </div>
           </div>
